b2f98cbfff7457e08a87f7f48c5bb3e157865463
Madel dela Cruz <madeldelacruz@Madels-MacBook-Pro.local>
2021-12-27 12:32:33 +1300

    a xylophone app fin.

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
+  void playSound(int soundNumber) {
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey(Color buttonColor, int soundNumber) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(backgroundColor: buttonColor),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: null,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.blue, 5),
+              buildKey(Colors.indigo, 6),
+              buildKey(Colors.purple, 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
